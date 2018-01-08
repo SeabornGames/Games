618a6e3c2ea8bc8215e3c0a635e5413874841534
Ben Christenson <ben.christenson@xperi.com>
2018-01-07 19:52:45 -0600

    refactored git command

--- seaborn/games/git_commands.py
+++ seaborn/games/git_commands.py
@@ -1,58 +1,77 @@
-import os, sys
+import os
+import sys
 from subprocess import *
 
-PATH = os.path.abspath(__file__).replace('\\','/')
-DIS_PATH = PATH.split('/')
 
-i = None
-for i in range(len(DIS_PATH)):
-    if DIS_PATH[-i]=='games':
-        depth = i
+PATH = os.path.dirname(os.path.abspath(__file__))
+SUPER_PATH = os.path.dirname(PATH)
+SISTER_PATHS = [os.path.join(SUPER_PATH, i) for i in os.listdir(SUPER_PATH)
+                if '.' not in i and i != PATH]
 
-DEPTH = depth
 
-SUPER_PATH = '/'.join(DIS_PATH[:-DEPTH])
-SISTER_PATHS = [SUPER_PATH + '/' + i for i in os.listdir(SUPER_PATH)
-               if '.' not in i and i != DIS_PATH[-DEPTH]]
+
+
+
+def cmd(command):
+    return check_output(command, shell=True)
+
 
 def status(echo=True, *args):
-    result = check_output('git status', shell=True)
+    result = cmd('git status')
     if echo:
         print(result)
     return result
 
+
 def commit(*args):
-    commit = 'not staged' in seaborn_status(False)
+    commit = 'not staged' in status(False)
     if commit:
-        print(check_output('git add -A', shell=True))
-        print(check_output('git commit -m "%s"' % args[0], shell=True))
+        print(cmd('git add -A'))
+        print(cmd('git commit -m "%s"' % args[0]))
     else:
         print('Not committing: Everything up-to-date\n')
     return commit
 
+
 def push(*args):
-    push = 'up-to-date' in seaborn_status(False)
+    push = 'up-to-date' in status(False)
     if push:
-        check_output('git push', shell=True)
+        cmd('git push')
+
+
+def install(*args):
+    with open('setup.py', 'r') as fp:
+        name = fp.read().split("name='")[-1].split("'")[0]
+    cmd('pip uninstall %s'%name)
+    cmd('pip install -e .')
 
 
 def seaborn_status():
     dir_iter(status, sys.argv[1:])
 
+
 def seaborn_commit():
     dir_iter(commit, sys.argv[1:])
+
 
 def seaborn_push():
     dir_iter(push, sys.argv[1:])
 
-def dir_iter(func,*args):
+
+def seaborn_install():
+    dir_iter(install, sys.argv[1:])
+    dir_iter(install, sys.argv[1:])
+
+
+def dir_iter(func, *args):
     for path in SISTER_PATHS:
         os.chdir(path)
-        print('\n\n'+path)
+        print('\n\n' + path)
         try:
             func(*args)
         except BaseException as e:
             pass
+
 
 if __name__ == '__main__':
     dir_iter(eval(sys.argv[1]), sys.argv[2:])
