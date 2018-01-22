6a4c0b9ff2a5191f994a97f600f23b54c1f70929
MichaelChristenson <mochristenson@gmail.com>
2018-01-22 13:34:19 -0600

    Fixed issue with seaborn_status

--- seaborn/games/git_commands.py
+++ seaborn/games/git_commands.py
@@ -23,6 +23,12 @@ def func_iter(func):
 
     return ret
 
+def status(echo=True):
+    check_output('git fetch origin')
+    result = check_output('git status', shell=True)
+    if echo:
+        print(result.decode('utf-8'))
+    return result
 
 @func_iter
 def seaborn_install(*args):
@@ -30,16 +36,12 @@ def seaborn_install(*args):
     print(result.decode('utf-8'))
 
 @func_iter
-def seaborn_status(echo=True, *args):
-    result = check_output('git status', shell=True)
-    if echo:
-        print(result.decode('utf-8'))
-    return result
-
+def seaborn_status(*args):
+    return status()
 
 @func_iter
 def seaborn_commit(*args):
-    commit = 'not staged' in seaborn_status(False).decode('utf-8')
+    commit = 'not staged' in status(False).decode('utf-8')
     if commit:
         print(check_output('git add -A', shell=True).decode('utf-8'))
         print(check_output('git commit -m "%s"' % args[0], shell=True
@@ -51,7 +53,7 @@ def seaborn_commit(*args):
 
 @func_iter
 def seaborn_push(*args):
-    push = 'up-to-date' in seaborn_status(False).decode('utf-8')
+    push = 'up-to-date' in status(False).decode('utf-8')
     if push:
         print(check_output('git push', shell=True).decode('utf-8'))
     else:
@@ -61,7 +63,7 @@ def seaborn_push(*args):
 @func_iter
 def seaborn_pull(*args):
     mstr = '*master' in check_output('git branch').decode('utf-8')
-    pull = 'up-to-date' in seaborn_status(False).decode('utf-8')
+    pull = 'up-to-date' in status(False).decode('utf-8')
     if mstr and pull:
         print(check_output('git pull origin master').decode('utf-8'))
     else:
