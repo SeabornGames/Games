8f753f080b14a042190bef1a5ae28908d9476862
christensonb <seaborngames@gmail.com>
2018-02-17 21:32:08 -0600

    fixed setup

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
-from setuptools import setup, find_packages
+from setuptools import setup
+import os
+
 with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as f:
     long_description = f.read()
 
